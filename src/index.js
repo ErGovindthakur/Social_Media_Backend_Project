db763eb2f8b3545bd78a1b0f83d580980170392e
ErGovindthakur <ergovindthakur@gmail.com>
2024-10-29 12:15:27 +0530

    Deleting some unnecessary code

--- src/index.js
+++ src/index.js
@@ -7,33 +7,3 @@ import connectDB from "./db/db.js";
 
 dotenv.config({path:'./env'})
 connectDB()
-
-
-
-
-
-/*
-import express from "express"
-
-const app = new express()
-
-// Writing our iffe function
-(async() => {
-     try{
-         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-
-         app.on("error", (error) => {
-          console.log("Error : ", error)
-          throw error
-         })
-
-         app.listen(process.env.PORT, () => {
-          console.log(`App is listening on PORT ${process.env.PORT}`)
-         })
-     }
-     catch(err){
-          console.log("ERR",err.message)
-          throw err
-     }
-})()
-*/

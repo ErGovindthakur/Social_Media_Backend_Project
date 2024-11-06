ef7c875065f0b3eddba456dc7e782d310d2f3582
ErGovindthakur <ergovindthakur@gmail.com>
2024-11-06 18:41:19 +0530

    Learnt about Middlewares ApiError, ApiResponse

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
+const port = process.env.PORT || 7000;
 
 dotenv.config({path:'./env'})
 connectDB()
+.then(() => {
+     app.listen(port, ()=>{
+          console.log(`Server is running at port ${port}`)
+     })
+})
+.catch((err) => {
+     console.log("Mongo db connection failed !!")
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (fn) => {
+     (req,res,next) => {
+          Promise.resolve(fn(req,res,next)).catch((err) => next(err))
+     }
+}  
+
+// Creating a Higher order function
+
+// const asyncHandler = (fn) => async(req,res,next) => {
+//      try {
+//           await fn(req,res,next)
+//      } 
+//      catch (error) {
+//           res.status(error.code || 500).json({
+//                success:false,
+//                message:error.message
+//           })
+//      }
+// } 
+
+export {asyncHandler}
